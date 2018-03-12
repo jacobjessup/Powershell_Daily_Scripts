7359980bbe3d6509816f3f7a323ef0e05b7ff206
jacobjessup <jacobwjessup@gmail.com>
2018-03-11 23:24:48 -0400

    Create template.ps1

---
+++ template.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+        This is a template script and this is where you would type a synopsis of the command.
+
+.DESCRIPTION
+        This is a template script and this is where you would type a more detailed description of the command.
+
+.EXAMPLE
+        This is a template script and this is where you would type your first example of how to use this command.
+
+.EXAMPLE
+        This is a template script and this is where you would type your second example of how to use this command.
+
+#>
+
+#Written by Jacob Jessup on 3/1/2016
+
+#Initialization
+#This section contains variable declarations and assignments
+
+#Functions
+#This section contains function definitions
+
+#Main Body
+#This section contains the code that implements that main function of the script
